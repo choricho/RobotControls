9ba90f9bff259aa6d1f421ff65c806610afe45ed
choricho <choricho#davan.com>
2025-05-01 13:14:46 +0300

    Initial commit: added basic robot motor control script

--- motors_control.py
+++ motors_control.py
@@ -1,18 +1,22 @@
 # motors_control.py
 
-class RobotMotors:
+class Robot:
     def __init__(self):
         self.speed = 0
 
     def set_speed(self, speed):
-        if speed < 0:
-            self.speed = 0
-        elif speed > 100:
-            self.speed = 100
-        else:
+        if 0 <= speed <= 100:
             self.speed = speed
-        print(f"Motor speed set to {self.speed}%")
+            print(f"Speed set to {self.speed}%")
+        else:
+            print("Speed must be between 0 and 100.")
+
+    def stop(self):
+        self.speed = 0
+        print("Robot stopped.")
 
 if __name__ == "__main__":
-    robot = RobotMotors()
+    robot = Robot()
     robot.set_speed(50)
+    robot.stop()
+
